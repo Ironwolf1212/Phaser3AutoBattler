a691311eba17405b248b0ed139faf52abde10b22
Miguel <ironguy1212.mave@gmail.com>
2023-01-18 15:21:09 -0500

    Test example game

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const fixtures = path.resolve(__dirname, 'fixtures');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
+    },
   module: {
     rules: [
-    ],
-  },
+      ],
+      
+    },
+    
 };
